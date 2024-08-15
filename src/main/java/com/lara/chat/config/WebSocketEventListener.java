bbdad1fb4b25054b6d68edb78dfd42c33534c244
LasandiRandini <randini-im20057@stu.kln.ac.lk>
2024-08-15 21:31:50 +0530

    front end started

--- src/main/java/com/lara/chat/config/WebSocketEventListener.java
+++ src/main/java/com/lara/chat/config/WebSocketEventListener.java
@@ -1,9 +1,12 @@
 package com.lara.chat.config;
 
 
+import com.lara.chat.chat.ChatMessage;
+import com.lara.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -12,15 +15,23 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Component
 @Slf4j
 public class WebSocketEventListener {
-
+    private final SimpMessageSendingOperations messageTemplate;
     //we want to inform if a user in the chat application has left the chat
-@EventListener
+
+    @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
-    ){
-    StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-
-String username = (String) headerAccessor.getSessionAttributes().get("usename");
-if (username! = null)  {
-    log.info("User disconnected: {} ", username
+    ) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("usename");
+        if (username != null) {
+            log.info("User disconnected: {} ", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            //destination that user msges go and what the msg
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
+}
